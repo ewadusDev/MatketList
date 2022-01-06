231c4a1daad4f113b94c09ac94d285134413b8e9
Ewadus <rtkeosung@gmail.com>
2022-01-06 19:43:27 +0700

    add splash screen

---
+++ MarketList/app/src/main/java/com/ewadus/marketlist/ui/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package com.ewadus.marketlist.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.ewadus.marketlist.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed(
+            {
+                val intent = Intent(this,RegisterActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            },3000
+        )
+
+    }
+}
